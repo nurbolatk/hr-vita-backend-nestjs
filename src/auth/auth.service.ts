8393c83330672ec29ad91fc84dbfb62645903f0f
nurbolatk <nurbolattk@gmail.com>
2022-03-25 04:26:22 +0600

    feat: create jwt tokens and check with guard

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,9 +7,15 @@ import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { User } from '@prisma/client';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 @Injectable({})
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async signin(dto: AuthDto) {
     const user: User | null = await this.prisma.user.findUnique({
@@ -26,7 +32,7 @@ export class AuthService {
     return user;
   }
 
-  async signup(dto: AuthDto): Promise<Omit<User, 'hash'>> {
+  async signup(dto: AuthDto): Promise<string> {
     const hash: string = await argon.hash(dto.password);
     const user: User = await this.prisma.user.create({
       data: {
@@ -34,7 +40,18 @@ export class AuthService {
         hash,
       },
     });
-    delete user.hash;
-    return user;
+
+    return this.signToken(user.id, user.email);
+  }
+
+  private async signToken(userId: number, email: string): Promise<string> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    return this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: this.config.get('JWT_SECRET'),
+    });
   }
 }
