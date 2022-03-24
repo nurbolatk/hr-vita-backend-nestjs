d8c39c372ee05cc41d19c67059a8ec2f56f393b9
nurbolatk <nurbolattk@gmail.com>
2022-03-25 00:40:41 +0600

    feat: create login and signup logic w/o jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
-    return this.authService.signup();
+  signup(@Body() dto: AuthDto) {
+    console.log(dto);
+
+    return this.authService.signup(dto);
+  }
+
+  @Post('signin')
+  signin(@Body() dto: AuthDto) {
+    console.log(dto);
+
+    return this.authService.signin(dto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,40 @@
-import { Injectable } from '@nestjs/common';
-import { User } from '.prisma/client';
-
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { User } from '@prisma/client';
 @Injectable({})
 export class AuthService {
-  login() {}
-  signup() {}
+  constructor(private prisma: PrismaService) {}
+
+  async signin(dto: AuthDto) {
+    const user: User | null = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (!user) throw new BadRequestException('Invalid email or password');
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    if (!pwMatches) {
+      throw new ForbiddenException('Invalid email or password');
+    }
+    delete user.hash;
+    return user;
+  }
+
+  async signup(dto: AuthDto): Promise<Omit<User, 'hash'>> {
+    const hash: string = await argon.hash(dto.password);
+    const user: User = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+    delete user.hash;
+    return user;
+  }
 }
