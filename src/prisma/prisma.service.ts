b861c27bb112f324edf5c3d4966d090ad720b7e5
nurbolatk <nurbolattk@gmail.com>
2022-03-25 00:51:41 +0600

    feat: use ConfigModule to get db url

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'postgresql://postgres:123@localhost:5432/hr-vita-nest?schema=public',
+          url: config.get('DATABASE_URL'),
         },
       },
     });
