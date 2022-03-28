1d7421156b40127fbf6552f2be7acbd3bd3a5be0
nurbolatk <nurbolattk@gmail.com>
2022-03-28 23:05:47 +0600

    fix: make pos & dep unique!

--- src/candidate/candidate.service.ts
+++ src/candidate/candidate.service.ts
@@ -9,50 +9,29 @@ export class CandidateService {
 
   async create(candidate: CreateCandidateDto): Promise<Candidate> {
     try {
-      const [position, department] = await Promise.all([
-        await this.prisma.position.findFirst({
-          where: {
-            name: {
-              equals: candidate.position,
-            },
-          },
-        }),
-        await this.prisma.department.findFirst({
-          where: {
-            name: {
-              equals: candidate.department,
-            },
-          },
-        }),
-      ]);
-
       const newCandidate = await this.prisma.candidate.create({
         data: {
           ...candidate,
-          position:
-            position !== null
-              ? {
-                  connect: {
-                    id: position.id,
-                  },
-                }
-              : {
-                  create: {
-                    name: candidate.position,
-                  },
-                },
-          department:
-            department === null
-              ? {
-                  create: {
-                    name: candidate.department,
-                  },
-                }
-              : {
-                  connect: {
-                    id: department.id,
-                  },
-                },
+          position: {
+            connectOrCreate: {
+              where: {
+                name: candidate.position,
+              },
+              create: {
+                name: candidate.position,
+              },
+            },
+          },
+          department: {
+            connectOrCreate: {
+              where: {
+                name: candidate.department,
+              },
+              create: {
+                name: candidate.department,
+              },
+            },
+          },
         },
         include: {
           position: true,
