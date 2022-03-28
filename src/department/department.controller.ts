72f6723c769086a96ce156dd6a9ee3e5237444a9
nurbolatk <nurbolattk@gmail.com>
2022-03-28 20:05:24 +0600

    feat: department service getAll & create

--- src/department/department.controller.ts
+++ src/department/department.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Department } from '.prisma/client';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { JwtGuard } from 'src/auth/guard';
 import { DepartmentService } from './department.service';
+import { CreateDepartmentDto } from './dto/createDepartment.dto';
 
 @Controller('departments')
 export class DepartmentController {
   constructor(private service: DepartmentService) {}
+
+  @Get()
+  getAll(): Promise<Department[]> {
+    return this.service.getAll();
+  }
+
+  @UseGuards(JwtGuard)
+  @Post()
+  createPosition(@Body() dto: CreateDepartmentDto) {
+    // TODO: check role
+    return this.service.create(dto);
+  }
 }
--- src/department/department.service.ts
+++ src/department/department.service.ts
@@ -1,7 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateDepartmentDto } from './dto/createDepartment.dto';
 
 @Injectable()
 export class DepartmentService {
   constructor(private prisma: PrismaService) {}
+  getAll() {
+    return this.prisma.department.findMany();
+  }
+
+  create(dto: CreateDepartmentDto) {
+    return this.prisma.department.create({
+      data: dto,
+    });
+  }
 }
